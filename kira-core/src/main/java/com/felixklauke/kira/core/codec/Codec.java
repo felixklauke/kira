7ad4a154b435a8f4bd8a561fd2bd18feafd24eb9
Felix Klauke <felix@klauke-enterprises.com>
2022-07-12 09:20:44 +0200

    Feature/cleanup (#82)
    * Cleanup gradle project
    
    * Accept gradle enterprise

--- kira-core/src/main/java/com/felixklauke/kira/core/codec/Codec.java
+++
@@ -1,9 +0,0 @@
-package com.felixklauke.kira.core.codec;
-
-import com.felixklauke.kira.core.KiraCodecException;
-
-public interface Codec<PropertyT> {
-  Object serialize(PropertyT value) throws KiraCodecException;
-
-  PropertyT deserialize(Object value) throws KiraCodecException;
-}
